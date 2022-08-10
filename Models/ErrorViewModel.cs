82f832cc9058fd3f1e7a7a87081f156f3ce2e12d
Diana Araujo <dianaaraujodasilva04@gmail.com>
2022-08-10 10:21:21 -0300

    dotnet

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExemploAsNetMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
